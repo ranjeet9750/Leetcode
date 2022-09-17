b95183726e2bdc96cc1dcbfeadf5462a3f6708af
ranjeet9750 <53788419+ranjeet9750@users.noreply.github.com>
2022-09-17 23:23:26 +0530

    Time: 51 ms (5.79%), Space: 13.2 MB (54.07%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -2,30 +2,70 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         
-        int m = matrix.size();
-        int n = matrix[0].size();
-        vector<vector<int>> mat(m, vector<int>(n));
+       int row = matrix.size();
+       int col = matrix[0].size();
+        bool isCol = false;
         
-        for(int i = 0 ; i < m; i++){
-            for(int j = 0; j < n; j++){
-                mat[i][j] = matrix[i][j];
-                }
+        for(int i = 0 ; i <row ; i++){
+            if(matrix[i][0]==0){
+                isCol = true;
+        }
+        
+        for(int j = 1 ; j < col ; j++){
+            
+            if(matrix[i][j]==0){
+                matrix[0][j]=0;
+                matrix[i][0]=0;
             }
-        for(int i = 0; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
-                if(matrix[i][j]==0){
-                    for(int i = 0; i < m; i++)
-                        mat[i][j]=0;
-                    for(int j = 0 ; j < n ; j++)
-                        mat[i][j]=0;
+        }
+        }
+        
+        for(int i = 1; i < row; i++){
+            for(int j = 1; j < col ; j++){
+                if(matrix[i][0]==0|| matrix[0][j]==0){
+                    matrix[i][j] = 0 ;
                 }
             }
         }
-        for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
-                matrix[i][j]=mat[i][j];
+        
+        if(matrix[0][0]==0){
+            for(int j = 0 ; j< col ; j++){
+                matrix[0][j]=0;
             }
         }
-     
+        
+        if(isCol){
+            for(int i = 0 ; i < row; i++){
+                matrix[i][0]= 0;
+            }
+        }
+        
+        
     }
 };
+//solution 1
+// int m = matrix.size();
+//         int n = matrix[0].size();
+//         vector<vector<int>> mat(m, vector<int>(n));
+        
+//         for(int i = 0 ; i < m; i++){
+//             for(int j = 0; j < n; j++){
+//                 mat[i][j] = matrix[i][j];
+//                 }
+//             }
+//         for(int i = 0; i < m ; i++){
+//             for(int j = 0 ; j < n ; j++){
+//                 if(matrix[i][j]==0){
+//                     for(int i = 0; i < m; i++)
+//                         mat[i][j]=0;
+//                     for(int j = 0 ; j < n ; j++)
+//                         mat[i][j]=0;
+//                 }
+//             }
+//         }
+//         for(int i = 0 ; i < m ; i++){
+//             for(int j = 0 ; j < n ; j++){
+//                 matrix[i][j]=mat[i][j];
+//             }
+//         }
+     
