67b51667f2d53a2400b60f796cc5e2c94a7ee88e
ranjeet9750 <53788419+ranjeet9750@users.noreply.github.com>
2022-09-18 11:29:25 +0530

    Time: 19 ms (74.98%), Space: 13.3 MB (54.14%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,71 +1,31 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
+        int isCol = 1;
+        int rows = matrix.size();
+        int cols = matrix[0].size();
         
-       int row = matrix.size();
-       int col = matrix[0].size();
-        bool isCol = false;
-        
-        for(int i = 0 ; i <row ; i++){
-            if(matrix[i][0]==0){
-                isCol = true;
-        }
-        
-        for(int j = 1 ; j < col ; j++){
-            
-            if(matrix[i][j]==0){
-                matrix[0][j]=0;
-                matrix[i][0]=0;
-            }
-        }
-        }
-        
-        for(int i = 1; i < row; i++){
-            for(int j = 1; j < col ; j++){
-                if(matrix[i][0]==0|| matrix[0][j]==0){
-                    matrix[i][j] = 0 ;
+        for(int i = 0 ; i < rows ; i++){
+            if(matrix[i][0]==0)
+                isCol = 0 ; 
+            for(int j = 1 ; j < cols ; j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
                 }
             }
         }
         
-        if(matrix[0][0]==0){
-            for(int j = 0 ; j< col ; j++){
-                matrix[0][j]=0;
+        for(int i = rows-1; i >=0 ; i--){
+            for(int j = cols-1; j >=1; j--){
+                if(matrix[i][0]==0|| matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
             }
-        }
-        
-        if(isCol){
-            for(int i = 0 ; i < row; i++){
-                matrix[i][0]= 0;
-            }
+            if(isCol == 0)
+                matrix[i][0]=0;
         }
         
         
     }
 };
-//solution 1
-// int m = matrix.size();
-//         int n = matrix[0].size();
-//         vector<vector<int>> mat(m, vector<int>(n));
-        
-//         for(int i = 0 ; i < m; i++){
-//             for(int j = 0; j < n; j++){
-//                 mat[i][j] = matrix[i][j];
-//                 }
-//             }
-//         for(int i = 0; i < m ; i++){
-//             for(int j = 0 ; j < n ; j++){
-//                 if(matrix[i][j]==0){
-//                     for(int i = 0; i < m; i++)
-//                         mat[i][j]=0;
-//                     for(int j = 0 ; j < n ; j++)
-//                         mat[i][j]=0;
-//                 }
-//             }
-//         }
-//         for(int i = 0 ; i < m ; i++){
-//             for(int j = 0 ; j < n ; j++){
-//                 matrix[i][j]=mat[i][j];
-//             }
-//         }
-     
